fce3f0e08282b6c283ee3486a84aef2d21e69d30
Gabriel Mougard <gabriel.mougard@gmail.com>
2022-11-30 20:12:56 +0100

    Successful MariaDB connection

--- forensicpp/src/forensicpp/main.cc
+++ forensicpp/src/forensicpp/main.cc
@@ -1,12 +1,39 @@
 #include <memory>
+#include <iostream>
+#include <cstring>
+#include <mariadb/conncpp.hpp>
 
 #include <ftxui/component/screen_interactive.hpp>  // for Component, ScreenInteractive
 
 #include "forensicpp/ui/main_component.h"
+#include "forensicpp/db/credentials.h"
 
 using namespace ftxui;
 
 int main(int argc, const char* argv[]) {
+  // TODO : CLI parsing
+  
+  /*
+    Connect to MariaDB (TODO : move it somewhere else.
+    Typically, `conn` should be member of the `Manager` class
+  */
+  try {
+    // Instantiate Driver
+    sql::Driver* driver = sql::mariadb::get_driver_instance();
+
+    // Configure Connection
+    std::string database_access = "jdbc:mariadb://" + std::string(DEFAULT_MARIADB_FORENSICK_HOST) + ":" + std::string(DEFAULT_MARIADB_FORENSICK_PORT) + "/" + std::string(DEFAULT_MARIADB_FORENSICK_DATABASE);  
+    sql::SQLString url(database_access.c_str());
+    sql::Properties properties({{"user", DEFAULT_MARIADB_FORENSICK_USER}, {"password", DEFAULT_MARIADB_FORENSICK_PASSWORD}});
+
+    // Establish Connection
+    std::unique_ptr<sql::Connection> conn(driver->connect(url, properties));
+  } catch(sql::SQLException& e) {
+    std::cerr << "Error Connecting to MariaDB Platform: " << e.what() << std::endl;
+    // Exit (Failed)
+    return 1;
+  }
+
   auto screen = ScreenInteractive::Fullscreen();
   auto ui = std::make_shared<MainComponent>();
 
